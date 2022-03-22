984bb6ebda4348710b1b36a3f1b5cfe658b77339
梁灏 <admin@aresn.com>
2022-03-22 18:45:50 +0800

    add vite build

---
+++ vite.config.js
@@ -0,0 +1,53 @@
+import {defineConfig} from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+const path = require('path');
+
+// https://vitejs.dev/config/
+const config = defineConfig({
+    plugins: [vue()],
+    target: 'es2015',
+    build: {
+        outDir: path.resolve(__dirname, './dist'),
+        lib: {
+            entry: path.resolve(__dirname, './src/index.js'),
+            name: 'ViewUIPlus'
+        },
+        rollupOptions: {
+            context: 'globalThis',
+            preserveEntrySignatures: 'strict',
+            external: ['vue'],
+            output: [
+                {
+                    format: 'umd',
+                    exports: 'named',
+                    sourcemap: false,
+                    entryFileNames: 'viewuiplus.js',
+                    chunkFileNames: '[name].js',
+                    assetFileNames: '[name].[ext]',
+                    namespaceToStringTag: true,
+                    inlineDynamicImports: false,
+                    manualChunks: undefined,
+                    globals: {vue: 'Vue'}
+                },
+                {
+                    format: 'es',
+                    exports: 'named',
+                    sourcemap: false,
+                    entryFileNames: 'viewuiplus.esm.js',
+                    chunkFileNames: '[name].js',
+                    assetFileNames: '[name].[ext]',
+                    namespaceToStringTag: true,
+                    inlineDynamicImports: false,
+                    manualChunks: undefined,
+                    globals: {vue: 'Vue'}
+                }
+            ]
+        }
+    },
+    resolve: {
+        extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    }
+});
+
+export default config;
