f548dcbbf5e75f5436b1d23c6acc1ddfb7f206a7
Aresn <admin@aresn.com>
2022-03-25 12:39:59 +0800

    create mixins: globalConfig

---
+++ src/mixins/globalConfig.js
@@ -0,0 +1,12 @@
+import { getCurrentInstance } from 'vue';
+export default {
+    data () {
+        return {
+            globalConfig: {}
+        }
+    },
+    created () {
+        const instance = getCurrentInstance();
+        this.globalConfig = instance.appContext.config.globalProperties.$IVIEW;
+    }
+}
