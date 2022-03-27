4d048ab25710a7b4b6837096a3e70bbb0c94ca74
梁灏 <admin@aresn.com>
2022-03-27 19:25:53 +0800

    update Dropdown import

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -1,5 +1,3 @@
 import Dropdown from './dropdown.vue';
-import DropdownMenu from './dropdown-menu.vue';
-import DropdownItem from './dropdown-item.vue';
 
-export { Dropdown, DropdownMenu, DropdownItem };
+export default Dropdown;
