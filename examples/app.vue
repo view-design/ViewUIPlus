682ba562651807265b9a6e4a9a0a9943fcea8ac3
Aresn <admin@aresn.com>
2022-03-29 10:51:50 +0800

    Update app.vue

--- examples/app.vue
+++ examples/app.vue
@@ -16,7 +16,7 @@
             <span>[todo]<router-link to="/anchor">Anchor</router-link></span>
             <span><router-link to="/grid">Grid</router-link></span>
             <span><router-link to="/button">Button</router-link></span>
-            <span>[todo]<router-link to="/input">Input</router-link></span>
+            <span><router-link to="/input">Input</router-link></span>
             <span>[todo]<router-link to="/radio">Radio</router-link></span>
             <span>[todo]<router-link to="/checkbox">Checkbox</router-link></span>
             <span>[todo]<router-link to="/steps">Steps</router-link></span>
@@ -48,7 +48,7 @@
 <!--            <span><router-link to="/page">Page</router-link></span>-->
             <span>[todo]<router-link to="/transfer">Transfer</router-link></span>
 <!--            <span><router-link to="/date">Date</router-link></span>-->
-            <span>[todo]<router-link to="/form">Form</router-link></span>
+            <span><router-link to="/form">Form</router-link></span>
 <!--            <span><router-link to="/table">Table</router-link></span>-->
             <span><router-link to="/loading-bar">LoadingBar</router-link></span>
             <span>[todo]<router-link to="/modal">Modal</router-link></span>
