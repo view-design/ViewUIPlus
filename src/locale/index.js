f0897e2844180390dba8aebd59cdeb916fd2e82c
Hunter <273676636@qq.com>
2022-04-19 17:53:36 +0800

    config i18n

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,26 +1,34 @@
-import defaultLang from './lang/zh-CN';
-import Vue from 'vue';
+import zhLang from './lang/zh-CN';
+import enLang from './lang/en-US';
 import deepmerge from 'deepmerge';
 import Format from './format';
 
-const format = Format(Vue);
-let lang = defaultLang;
-let merged = false;
-let i18nHandler = function() {
-    const vuei18n = Object.getPrototypeOf(this || Vue).$t;
-    if (typeof vuei18n === 'function' && !!Vue.locale) {
-        if (!merged) {
-            merged = true;
-            Vue.locale(
-                Vue.config.lang,
-                deepmerge(lang, Vue.locale(Vue.config.lang) || {}, { clone: true })
-            );
+const format = Format();
+let lang = zhLang;
+const langs = {
+    zh: zhLang,
+    en: enLang
+};
+let nowLang = null;
+let merged = {};
+let vuei18n = null;
+let i18nHandler = function () {
+    if (Reflect.has(this, '$t')) return this.$t(...arguments);
+    if (vuei18n && vuei18n.locale) {
+        if (!merged[vuei18n.locale] || nowLang != vuei18n.locale) {
+            merged[vuei18n.locale] = true;
+            let localMessage = vuei18n.getLocaleMessage(vuei18n.locale) || {};
+            let newLocalMessage = {};
+            deepmerge(newLocalMessage, langs[vuei18n.locale], localMessage, { clone: true })
+            lang = newLocalMessage;
+            vuei18n.setLocaleMessage(vuei18n.locale, newLocalMessage);
+            nowLang = vuei18n.locale;
         }
-        return vuei18n.apply(this, arguments);
+        return vuei18n.hlang(...arguments);
     }
 };
 
-export const t = function(path, options) {
+export const t = function (path, options) {
     let value = i18nHandler.apply(this, arguments);
     if (value !== null && value !== undefined) return value;
 
@@ -37,12 +45,16 @@ export const t = function(path, options) {
     return '';
 };
 
-export const use = function(l) {
+export const use = function (l) {
     lang = l || lang;
 };
 
-export const i18n = function(fn) {
-    i18nHandler = fn || i18nHandler;
+export const i18n = function (initI18n) {
+    vuei18n = initI18n;
 };
 
-export default { use, t, i18n };
+export default {
+    use,
+    t,
+    i18n
+};
