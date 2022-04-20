2ff17675da75aad61547ed88d9da4a880d40b437
Hunter <273676636@qq.com>
2022-04-20 15:21:58 +0800

    modify module export

--- src/index.js
+++ src/index.js
@@ -6,125 +6,9 @@
  * Released under the MIT license
  *
  */
-import Affix from './components/affix';
-import Alert from './components/alert';
-import Anchor from './components/anchor';
-import AnchorLink from './components/anchor-link';
-import Auth from './components/auth';
-import AutoComplete from './components/auto-complete';
-import Avatar from './components/avatar';
-import AvatarList from './components/avatar-list';
-import BackTop from './components/back-top';
-import Badge from './components/badge';
-import Breadcrumb from './components/breadcrumb';
-import BreadcrumbItem from './components/breadcrumb-item';
-import Button from './components/button';
-import ButtonGroup from './components/button-group';
-import Calendar from './components/calendar';
-import Captcha from './components/captcha';
-import Card from './components/card';
-import Carousel from './components/carousel';
-import CarouselItem from './components/carousel-item';
-import Cascader from './components/cascader';
-import Cell from './components/cell';
-import CellGroup from './components/cell-group';
-import Checkbox from './components/checkbox';
-import CheckboxGroup from './components/checkbox-group';
-import Circle from './components/circle';
-import City from './components/city';
-import Col from './components/col';
-import Collapse from './components/collapse';
-import ColorPicker from './components/color-picker';
-import Content from './components/content';
-import Copy from './components/copy';
-import CountDown from './components/count-down';
-import CountUp from './components/count-up';
-import DatePicker from './components/date-picker';
-import Description from './components/description';
-import DescriptionList from './components/description-list';
-import Divider from './components/divider';
-import Drawer from './components/drawer';
-import Dropdown from './components/dropdown';
-import DropdownItem from './components/dropdown-item';
-import DropdownMenu from './components/dropdown-menu';
-import Ellipsis from './components/ellipsis';
-import Email from './components/email';
-import Exception from './components/exception';
-import Footer from './components/footer';
-import FooterToolbar from './components/footer-toolbar';
-import Form from './components/form';
-import FormItem from './components/form-item';
-import GlobalFooter from './components/global-footer';
-import Grid from './components/grid';
-import GridItem from './components/grid-item';
-import Header from './components/header';
-import Icon from './components/icon';
-import Input from './components/input';
-import InputNumber from './components/input-number';
-import Layout from './components/layout';
-import List from './components/list';
-import ListItem from './components/list-item';
-import ListItemMeta from './components/list-item-meta';
-import LoadingBar from './components/loading-bar';
-import Login from './components/login';
-import Menu from './components/menu';
-import MenuGroup from './components/menu-group';
-import MenuItem from './components/menu-item';
-import Message from './components/message';
-import Mobile from './components/mobile';
-import Modal from './components/modal';
-import Notice from './components/notice';
-import Notification from './components/notification';
-import NotificationItem from './components/notification-item';
-import NotificationTab from './components/notification-tab';
-import NumberInfo from './components/number-info';
-import Numeral from './components/numeral';
-import Option from './components/option';
-import OptionGroup from './components/option-group';
-import Page from './components/page';
-import PageHeader from './components/page-header';
-import Panel from './components/panel';
-import Password from './components/password';
-import Poptip from './components/poptip';
-import Progress from './components/progress';
-import Radio from './components/radio';
-import RadioGroup from './components/radio-group';
-import Rate from './components/rate';
-import Result from './components/result';
-import Row from './components/row';
-import Scroll from './components/scroll';
-import ScrollIntoView from './components/scroll-into-view';
-import ScrollTop from './components/scroll-top';
-import Select from './components/select';
-import Sider from './components/sider';
-import Slider from './components/slider';
-import Spin from './components/spin';
-import Split from './components/split';
-import Step from './components/step';
-import Steps from './components/steps';
-import Submenu from './components/submenu';
-import Submit from './components/submit';
-import Switch from './components/switch';
-import Table from './components/table';
-import TablePaste from './components/table-paste';
-import TabPane from './components/tab-pane';
-import Tabs from './components/tabs';
-import Tag from './components/tag';
-import TagSelect from './components/tag-select';
-import TagSelectOption from './components/tag-select-option';
-import Time from './components/time';
-import Timeline from './components/timeline';
-import TimelineItem from './components/timeline-item';
-import TimePicker from './components/time-picker';
-import Tooltip from './components/tooltip';
-import Transfer from './components/transfer';
-import Tree from './components/tree';
-import TreeSelect from './components/tree-select';
-import Trend from './components/trend';
-import Upload from './components/upload';
-import UserName from './components/user-name';
-import WordCount from './components/word-count';
-import locale from './locale/index';
+export * from './components';
+import * as components from './components';
+import localeFile from './locale/index';
 
 // directives
 import lineClamp from './directives/line-clamp';
@@ -133,124 +17,6 @@ import style from './directives/style';
 
 // libraries
 import dayjs from 'dayjs';
-
-const components = {
-    Affix,
-    Alert,
-    Anchor,
-    AnchorLink,
-    Auth,
-    AutoComplete,
-    Avatar,
-    AvatarList,
-    BackTop,
-    Badge,
-    Breadcrumb,
-    BreadcrumbItem,
-    Button,
-    ButtonGroup,
-    Calendar,
-    Captcha,
-    Card,
-    Carousel,
-    CarouselItem,
-    Cascader,
-    Cell,
-    CellGroup,
-    Checkbox,
-    CheckboxGroup,
-    Circle,
-    City,
-    Col,
-    Collapse,
-    ColorPicker,
-    Content,
-    CountDown,
-    CountUp,
-    DatePicker,
-    Description,
-    DescriptionList,
-    Divider,
-    Drawer,
-    Dropdown,
-    DropdownItem,
-    DropdownMenu,
-    Ellipsis,
-    Email,
-    Exception,
-    Footer,
-    FooterToolbar,
-    Form,
-    FormItem,
-    GlobalFooter,
-    Grid,
-    GridItem,
-    Header,
-    Icon,
-    Input,
-    InputNumber,
-    Layout,
-    List,
-    ListItem,
-    ListItemMeta,
-    LoadingBar,
-    Login,
-    Menu,
-    MenuGroup,
-    MenuItem,
-    Message,
-    Mobile,
-    Modal,
-    Notice,
-    Notification,
-    NotificationItem,
-    NotificationTab,
-    NumberInfo,
-    Numeral,
-    Option,
-    OptionGroup,
-    Page,
-    PageHeader,
-    Panel,
-    Password,
-    Poptip,
-    Progress,
-    Radio,
-    RadioGroup,
-    Rate,
-    Result,
-    Row,
-    Scroll,
-    Select,
-    Sider,
-    Slider,
-    Split,
-    Spin,
-    Step,
-    Steps,
-    Submenu,
-    Submit,
-    Switch,
-    Table,
-    TablePaste,
-    Tabs,
-    TabPane,
-    Tag,
-    TagSelect,
-    TagSelectOption,
-    Time,
-    Timeline,
-    TimelineItem,
-    TimePicker,
-    Tooltip,
-    Transfer,
-    Tree,
-    TreeSelect,
-    Trend,
-    Upload,
-    UserName,
-    WordCount
-};
 
 const directives = {
     display: style.display,
@@ -267,30 +33,30 @@ const directives = {
 
 const iview = {
     ...components,
-    iButton: Button,
-    iCircle: Circle,
-    iCol: Col,
-    iContent: Content,
-    iForm: Form,
-    iFooter: Footer,
-    iHeader: Header,
-    iInput: Input,
-    iMenu: Menu,
-    iOption: Option,
-    iProgress: Progress,
-    iSelect: Select,
-    iSwitch: Switch,
-    iTable: Table,
-    iTime: Time
+    iButton: components.Button,
+    iCircle: components.Circle,
+    iCol: components.Col,
+    iContent: components.Content,
+    iForm: components.Form,
+    iFooter: components.Footer,
+    iHeader: components.Header,
+    iInput: components.Input,
+    iMenu: components.Menu,
+    iOption: components.Option,
+    iProgress: components.Progress,
+    iSelect: components.Select,
+    iSwitch: components.Switch,
+    iTable: components.Table,
+    iTime: components.Time
 };
 
-const install = function(app, opts = {}) {
+export const install = function(app, opts = {}) {
     if (install.installed) return;
     if (opts.locale) {
-        locale.use(opts.locale);
+        localeFile.use(opts.locale);
     }
     if (opts.i18n) {
-        locale.i18n(opts.i18n);
+        localeFile.i18n(opts.i18n);
     }
 
     Object.keys(iview).forEach(key => {
@@ -358,14 +124,14 @@ const install = function(app, opts = {}) {
             iconSize: opts.timePicker ? opts.timePicker.iconSize ? opts.timePicker.iconSize : '' : ''
         }
     }
-    app.config.globalProperties.$Spin = Spin;
-    app.config.globalProperties.$Loading = LoadingBar;
-    app.config.globalProperties.$Message = Message;
-    app.config.globalProperties.$Notice = Notice;
-    app.config.globalProperties.$Modal = Modal;
-    app.config.globalProperties.$Copy = Copy;
-    app.config.globalProperties.$ScrollIntoView = ScrollIntoView;
-    app.config.globalProperties.$ScrollTop = ScrollTop;
+    app.config.globalProperties.$Spin = components.Spin;
+    app.config.globalProperties.$Loading = components.LoadingBar;
+    app.config.globalProperties.$Message = components.Message;
+    app.config.globalProperties.$Notice = components.Notice;
+    app.config.globalProperties.$Modal = components.Modal;
+    app.config.globalProperties.$Copy = components.Copy;
+    app.config.globalProperties.$ScrollIntoView = components.ScrollIntoView;
+    app.config.globalProperties.$ScrollTop = components.ScrollTop;
 
     app.config.globalProperties.$Date = dayjs;
 };
@@ -375,21 +141,27 @@ if (typeof window !== 'undefined' && window.Vue) {
     install(window.Vue);
 }
 
+export const version = process.env.VERSION;
+
+export const locale = localeFile.use;
+
+export const i18n = localeFile.i18n;
+
+export const lang = (code) => {
+    const langObject = window['viewuiplus/locale'].default;
+    if (code === langObject.i.locale) localeFile.use(langObject);
+    else console.log(`The ${code} language pack is not loaded.`); // eslint-disable-line no-console
+};
+
 const API = {
-    version: process.env.VERSION, // eslint-disable-line no-undef
-    locale: locale.use,
-    i18n: locale.i18n,
+    version,
+    locale,
+    i18n,
     install,
+    lang,
     // Circle,
     // Switch,
     ...components
 };
 
-API.lang = (code) => {
-    const langObject = window['viewuiplus/locale'].default;
-    if (code === langObject.i.locale) locale.use(langObject);
-    else console.log(`The ${code} language pack is not loaded.`); // eslint-disable-line no-console
-};
-
-module.exports.default = module.exports = API;   // eslint-disable-line no-undef
-// export default API;
+export default API;
