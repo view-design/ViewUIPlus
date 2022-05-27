1dad90a6de1a50c27ece6d090d3f5292095630e9
Hunter <273676636@qq.com>
2022-05-27 20:27:37 +0800

    fix: types bug

--- types/select.d.ts
+++ types/select.d.ts
@@ -2,7 +2,7 @@ export declare class Select {
     /**
      * 指定选中项目的 value 值，可以使用 v-model 双向绑定数据。单选时只接受 String 或 Number，多选时只接受 Array|String | Number | Array
      */
-    'model-value': ;
+    'model-value': '';
 
     /**
      * 是否支持多选
