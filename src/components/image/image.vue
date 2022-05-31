1e9b9d044edc597b8487fffb18cf97b3c0dc8b86
https://github.com/DebugIsFalse <511418503@qq.com>
2022-06-01 00:21:43 +0800

    feat: add lazy event

--- src/components/image/image.vue
+++ src/components/image/image.vue
@@ -1,7 +1,9 @@
 <template>
-    <div :class="prefixCls">
+    <div :class="prefixCls" ref="image">
         <slot v-if="loading" name="placeholder">
-            <div :class="[prefixCls + '-placeholder']">LOADING</div>
+            <div :class="[prefixCls + '-placeholder']">
+                <span>LOADING</span>
+            </div>
         </slot>
         <slot v-else-if="imageError" name="error">
             <div :class="[prefixCls + '-error']">
@@ -22,6 +24,10 @@
     const prefixCls = 'ivu-image';
     import {on, off} from '../../utils/dom'
     import {isClient} from '../../utils/index';
+    // is Element
+    const isElement = (el)=> {
+        return typeof HTMLElement === 'object' && el instanceof HTMLElement;
+    }
     export default {
         name: 'Image',
         props: {
@@ -63,7 +69,8 @@
                 default: false
             },
             scrollContainer: {
-                type: [String, HTMLElement]
+                type: [String],
+                default: ''
             },
             transfer: {
                 type: Boolean,
@@ -89,7 +96,9 @@
                 imageError: false,
                 image: null,
                 imageWidth: 0,
-                imageHeight: 0
+                imageHeight: 0,
+                scrollElement: null,
+                observe: null
             }
         },
         computed: {
@@ -108,7 +117,38 @@
             isClient && this.handleImageEvent();
         },
         methods: {
+            // todo it can create new Observe to privide the function to use  in the future
+            handleLazy() {
+                const $el = this.$refs.image;
+                const observer = this.observe = new IntersectionObserver(this.handlerObserveImage, {
+                    root: this.scrollElement,
+                    rootMargin: "0px",
+                    threshold: 1
+                });
+                observer.observe($el);
+            },
+            handlerObserveImage (entries) {
+                for( let entry of entries) {
+                    if (entry.isIntersecting) {
+                        // destory new IntersectionObserver
+                        this.offObserve();
+                        // run image onload
+                        this.loadImage();
+                    }
+                }
+            },
             addLazyImageListener() {
+                const {scrollContainer} = this;
+                this.scrollElement = window;
+                if (isElement(scrollContainer)) {
+                    this.scrollElement = scrollContainer
+                } else if (typeof scrollContainer === 'string') {
+                    this.scrollElement = document.querySelector(scrollContainer);
+                }
+                this.loading = true;
+                this.imageError = false;
+                // on scrollElement scroll
+                this.handleLazy();
             },
             handleLoadImageLoad(event) {
                 const {width, height} = event.target;
@@ -116,13 +156,16 @@
                 this.imageHeight = height;
                 this.loading = false;
                 this.imageError = false;
-
-                this.$emit('on-load')
+                this.$emit('on-load');
+                // off image event
+                this.offImageEvent();
             },
             handleLoadImageError() {
                 this.loading = false;
                 this.imageError = true;
                 this.$emit('on-error');
+                // off image event
+                this.offImageEvent();
             },
             loadImage() {
                 this.image = new Image();
@@ -136,14 +179,22 @@
             handleImageEvent() {
                 const { lazy } = this;
                 lazy ? this.addLazyImageListener() : this.loadImage();
+            },
+            offImageEvent() {
+                const {image} = this;
+                image && off(image, 'load', this.handleLoadImageLoad);
+                image && off(image, 'error', this.handleLoadImageError)
+            },
+            offObserve() {
+                const {observe} = this;
+                if (observe) {
+                    observe.disconnect();
+                }
             }
         },
         beforeUnmount() {
-            const {image} = this;
-            if (image) {
-                off(image, 'load', this.handleLoadImageLoad);
-                off(image, 'error', this.handleLoadImageError)
-            }
+            this.offImageEvent();
+            this.offObserve();
         }
     }
 </script>
