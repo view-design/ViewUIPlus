02c1f630628101529b5c45b1bc93591ea51fc35e
Aresn <admin@aresn.com>
2022-06-01 16:18:10 +0800

    Update base.vue

--- src/components/typography/base.vue
+++ src/components/typography/base.vue
@@ -5,7 +5,7 @@
     export default {
         name: 'TypographyBase',
         mixins: [ baseProps ],
-        inheritAttrs: false,
+        // inheritAttrs: false,
         props: {
             component: {
                 type: String,
