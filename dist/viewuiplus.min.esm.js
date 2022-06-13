52377fc537c7a0bda1d1bf4cea5b90165cd6f915
Aresn <admin@aresn.com>
2022-06-13 10:01:38 +0800

    release 1.2.0

--- dist/viewuiplus.min.esm.js
+++ dist/viewuiplus.min.esm.js
@@ -37987,7 +37987,7 @@ var style = {
   }
 };
 const name = "view-ui-plus";
-const version$1 = "1.2.0-beta.4";
+const version$1 = "1.2.0";
 const title = "ViewUIPlus";
 const description = "A high quality UI components Library with Vue.js 3";
 const homepage = "http://www.iviewui.com";
