afa1f802dbb81aaab7d1e05c4964b4255e592170
Hunter <273676636@qq.com>
2022-06-15 10:45:37 +0800

    feat: Space

---
+++ examples/routers/space.vue
@@ -0,0 +1,28 @@
+<template>
+    <Space>
+        <Button>111</Button>
+        <span>444</span>
+        <!-- space -->
+    </Space>
+</template>
+
+<script>
+    import { resolveComponent, h } from 'vue';
+    export default {
+        data() {
+            return {
+                count: 333,
+                count1: 444,
+                count2: 555
+            }
+        },
+        // render() {
+        //     return h(resolveComponent('Space'), null, {
+        //         default: () => [this.count, null, undefined, []]
+        //     })
+        // }
+    }
+</script>
+
+<style scoped lang="less">
+</style>
