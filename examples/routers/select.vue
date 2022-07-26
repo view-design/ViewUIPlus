d965598b8de0dd802b8eaff2665ebc29fe69a4b1
DebugIsFalse <511418503@qq.com>
2022-07-26 14:37:25 +0800

    update Select example #145

--- examples/routers/select.vue
+++ examples/routers/select.vue
@@ -7,6 +7,32 @@
                 <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
             </template>
         </Select>
+        <Row :gutter="24" style="margin-top:10px">
+            <Col span="12">
+                <Select
+                    v-model="model"
+                    filterable
+                    :remote-method="remoteMethod3"
+                    default-label="北京"
+                    :loading="loading3"
+                >
+                    <Option v-for="(option, index) in options3" :value="option.value" :key="index">{{option.label}}</Option>
+                </Select>
+            </Col>
+            <Col span="12">
+                <Select
+                    v-model="model1"
+                    multiple
+                    filterable
+                    :remote-method="remoteMethod4"
+                    :default-label="['北京', '深圳']"
+                    :loading="loading4"
+                    @on-set-default-options="setDefaultOptions"
+                >
+                    <Option v-for="(option, index) in options4" :value="option.value" :key="index">{{option.label}}</Option>
+                </Select>
+            </Col>
+        </Row>
     </div>
 </template>
 <script>
@@ -40,7 +66,36 @@ export default {
                     label: 'Canberra'
                 }
             ],
-            model: ''
+            model: '',
+
+            model2: 'beijing',
+            options3: [],
+            loading3: false,
+            model1: ['beijing', 'shenzhen'],
+            options4: [],
+            loading4: false,
+            list2: [
+                {
+                    value: 'beijing',
+                    label: '北京'
+                },
+                {
+                    value: 'shanghai',
+                    label: '上海'
+                },
+                {
+                    value: 'shenzhen',
+                    label: '深圳'
+                },
+                {
+                    value: 'hangzhou',
+                    label: '杭州'
+                },
+                {
+                    value: 'guangzhou',
+                    label: '广州'
+                }
+            ]
         }
     },
     created(){
@@ -74,6 +129,31 @@ export default {
                     this.cityList = this.cityList.concat(randomArray);
                 }, 500)
             });
+        },
+        remoteMethod3 (query) {
+            if (query !== '') {
+                this.loading3 = true;
+                setTimeout(() => {
+                    this.loading3 = false;
+                    this.options3 = this.list2.filter(item => item.label.indexOf(query) > -1);
+                }, 200);
+            } else {
+                this.options3 = [];
+            }
+        },
+        remoteMethod4 (query) {
+            if (query !== '') {
+                this.loading4 = true;
+                setTimeout(() => {
+                    this.loading4 = false;
+                    this.options4 = this.list2.filter(item => item.label.indexOf(query) > -1);
+                }, 200);
+            } else {
+                this.options4 = [];
+            }
+        },
+        setDefaultOptions (options) {
+            this.options4 = options;
         }
     }
 }
